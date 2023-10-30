a9490c47110b903de2deb068793137b0f3d76deb
Cecile Thiebaut <cecile.thiebaut@gmail.com>
2023-10-30 08:45:15 +0100

    Bug when already in a pass

--- predict/pass_predictor.go
+++ predict/pass_predictor.go
@@ -1,39 +1,41 @@
-/**
-  predict4java: An SDP4 / SGP4 library for satellite orbit predictions
+/*
+*
 
-  Copyright (C)  2004-2010  David A. B. Johnson, G4DPZ.
+	predict4java: An SDP4 / SGP4 library for satellite orbit predictions
 
-  This class is a Java port of one of the core elements of
-  the Predict program, Copyright John A. Magliacane,
-  KD2BD 1991-2003: http://www.qsl.net/kd2bd/predict.html
+	Copyright (C)  2004-2010  David A. B. Johnson, G4DPZ.
 
-  Dr. T.S. Kelso is the author of the SGP4/SDP4 orbital models,
-  originally written in Fortran and Pascal, and released into the
-  public domain through his website (http://www.celestrak.com/).
-  Neoklis Kyriazis, 5B4AZ, later re-wrote Dr. Kelso's code in C,
-  and released it under the GNU GPL in 2002.
-  PREDICT's core is based on 5B4AZ's code translation efforts.
+	This class is a Java port of one of the core elements of
+	the Predict program, Copyright John A. Magliacane,
+	KD2BD 1991-2003: http://www.qsl.net/kd2bd/predict.html
 
-  Author: David A. B. Johnson, G4DPZ <dave@g4dpz.me.uk>
+	Dr. T.S. Kelso is the author of the SGP4/SDP4 orbital models,
+	originally written in Fortran and Pascal, and released into the
+	public domain through his website (http://www.celestrak.com/).
+	Neoklis Kyriazis, 5B4AZ, later re-wrote Dr. Kelso's code in C,
+	and released it under the GNU GPL in 2002.
+	PREDICT's core is based on 5B4AZ's code translation efforts.
 
-  Comments, questions and bugreports should be submitted via
-  http://sourceforge.net/projects/websat/
-  More details can be found at the project home page:
+	Author: David A. B. Johnson, G4DPZ <dave@g4dpz.me.uk>
 
-  http://websat.sourceforge.net
+	Comments, questions and bugreports should be submitted via
+	http://sourceforge.net/projects/websat/
+	More details can be found at the project home page:
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+	http://websat.sourceforge.net
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, visit http://www.fsf.org/
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, visit http://www.fsf.org/
 */
 package predict
 
@@ -142,7 +144,7 @@ func (p *PassPredictor) nextSatPassInternal(t time.Time, windBack bool, targetEl
 		// the horizon
 		for {
 			cal, satPos = p.getPosition(cal, 60)
-			if satPos.Elevation >= targetElevation {
+			if satPos.Elevation <= targetElevation {
 				break
 			}
 			if !cal.Before(stopSearch) {
